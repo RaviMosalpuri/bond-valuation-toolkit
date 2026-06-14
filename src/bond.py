4a4a91f814906feb87e3b17a844d4cd8ff76dcae
Ravi Mosalpuri <ravinmosalpuri@gmail.com>
2026-06-14 18:09:21 +0530

    [main] Bond valuation toolkit: 1. Added bond.py, requirements.txt 2. Modified .gitignore

---
+++ src/bond.py
@@ -0,0 +1,16 @@
+class Bond:
+    def __init__(self, face_value: float, coupon_rate: float, years_to_maturity: float, yield_to_maturity: float):
+        """
+        Initialize a bond.
+        face_value: Principal repayment at maturity
+        coupon_rate: Annual coupon rate (%)
+        years_to_maturity: Remaining years until maturity
+        yield_to_maturity: Market discount rate (%)
+        """
+        self.face_value = face_value
+        self.coupon_rate = coupon_rate
+        self.years_to_maturity = years_to_maturity
+        self.yield_to_maturity = yield_to_maturity
+
+    def annual_coupon(self):
+        return self.face_value * self.coupon_rate
